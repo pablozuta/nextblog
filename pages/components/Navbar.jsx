5ddceb678705eaa0c9ce7ae956e75e68f89ee458
Pablo Zuta <pablo.zuta.h@gmail.com>
2022-11-24 01:35:38 -0400

    se crea codigo para hacer toggle active class en navbar

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
 import Link from "next/link";
 
 
 function Navbar() {
+  const [btnState, setBtnState] = useState(false)
+
+  function handleClick(){
+    setBtnState(btnState => !btnState)
+  }
+
+  let toggleClassCheck = btnState ? ' active' : '';
+
   return (
     <div className='navbar'>
         <div className='logo'><Link href='/'>VALIS</Link></div>
 
-        <a href="#" class="toggle-button">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-
+        <a href="#" className="toggle-button" onClick={handleClick}>
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+     
     </a>
    
-    <div className="navbar-links">
+    <div className={`navbar-links${toggleClassCheck}`}>
         <ul>
             <li><Link href='/posts/web-technologies'>THE WIRE</Link></li>
             <li><Link href='/posts/astro'>ASTRO</Link></li>
